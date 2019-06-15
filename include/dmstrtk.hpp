251d6f2dc01f0e5379755060bf82aa3ab06e09e6
brinkqiang <brink.qiang@gmail.com>
2019-06-15 09:47:55 +0800

    add

--- include/dmstrtk.hpp
+++ include/dmstrtk.hpp
@@ -1630,39 +1630,6 @@ void remove_empty_strings( std::multiset<std::string, Comparator, Allocator>&
     }
 }
 
-//template <typename Iterator>
-//inline void replace( const typename std::iterator_traits<Iterator>::value_type&
-//                     c1,
-//                     const typename std::iterator_traits<Iterator>::value_type& c2,
-//                     const Iterator begin,
-//                     const Iterator end ) {
-//
-//    for ( Iterator itr = begin; end != itr; ++itr ) {
-//        if ( c1 == ( *itr ) ) {
-//            ( *itr ) = c2;
-//        }
-//    }
-//}
-
-inline void replace( const std::string::value_type& c0,
-                     const std::string::value_type& c1,
-                     std::string& s ) {
-    std::replace(std::begin(s), std::end(s), c0, c1);
-}
-
-inline std::string replace(const std::string& str0,
-    const std::string& str1,
-    std::string& s) {
-    std::string strOut = s;
-    replace_pattern(s, str0, str1, strOut);
-    return std::move(strOut);
-}
-
-template <typename T>
-inline void replace( const T& c1, const T& c2, const range::adapter<T>& r ) {
-    replace( c1, c2, r.begin(), r.end() );
-}
-
 inline void replace_pattern( const std::string& s, // input
                              const std::string& p, // pattern
                              const std::string& r, // replacement
@@ -1816,6 +1783,39 @@ inline std::size_t replace_pattern( const InputIterator s_begin,
 
     return new_size;
 }
+
+//template <typename Iterator>
+//inline void replace( const typename std::iterator_traits<Iterator>::value_type&
+//                     c1,
+//                     const typename std::iterator_traits<Iterator>::value_type& c2,
+//                     const Iterator begin,
+//                     const Iterator end ) {
+//
+//    for ( Iterator itr = begin; end != itr; ++itr ) {
+//        if ( c1 == ( *itr ) ) {
+//            ( *itr ) = c2;
+//        }
+//    }
+//}
+
+inline void replace(const std::string::value_type& c0,
+    const std::string::value_type& c1,
+    std::string& s) {
+    std::replace(std::begin(s), std::end(s), c0, c1);
+}
+
+inline std::string replace(const std::string& str0,
+    const std::string& str1,
+    const std::string& s) {
+    std::string strOut;
+    replace_pattern(s, str0, str1, strOut);
+    return std::move(strOut);
+}
+//
+//template <typename T>
+//inline void replace(const T& c1, const T& c2, const range::adapter<T>& r) {
+//    replace(c1, c2, r.begin(), r.end());
+//}
 
 inline void remove_pattern( const std::string& s,
                             const std::string& p,
